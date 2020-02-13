fd4ef651b42276d02190f45e45bb072a37158f76
Janu Sung <janu.m.sung@gmail.com>
2020-02-13 04:11:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,46 @@
+def push(arr, str)
+  arr.push(str)
+end
+
+def unshift(arr, str)
+  arr.unshift(str)
+end 
+
+def pop(arr)
+  arr.pop()
+end
+
+def pop_w_args(arr)
+  arr.pop(2)
+end 
+
+def shift(arr)
+  arr.shift()
+end 
+
+def shift_w_args(arr)
+  arr.shift(2)
+end 
+
+def concat(arr1, arr2)
+  arr1.concat(arr2)
+end 
+
+def insert(arr, ele)
+  arr.insert(4, ele)
+end 
+
+def uniq(arr)
+  arr.uniq()
+end 
+
+def flatten(arr)
+  arr.flatten()
+end 
+
+def delete(arr, str)
+  arr.delete(str)
+end 
+
+def delete_at(arr, idx)
+  arr.delete_at(idx)
